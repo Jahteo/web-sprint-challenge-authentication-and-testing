c2128d397c812c72114c8411815f955dd35f436d
Josh Glantz-Hucks <jahteo@gmail.com>
2020-10-16 11:19:15 -0700

    Finished testing. MVP & stretch of using a separate testing database successful.

--- api/server.spec.js
+++ api/server.spec.js
@@ -13,18 +13,49 @@ describe('index route', () => {
 //   // this function executes and clears out the table before each test
 //   await db('test').truncate();
 // });
+//beforeEach reset is broken, so be sure to make a new username in testee when you want this to run
+const testee = {
+  "username": "RealBoy17", "password": "Pinocchio"
+}
+const failureTestee = {
+  "username": "FakeBoy",
+}
 
 describe("auth-router.js", () => {
   describe("register route", () => {
-    it.skip("should return a {message: 'login post success'} on successful login", async () => {
-      //beforeEach reset is broken, so be sure to make a new username in testee when you want this to run
-      const testee = {
-        "username": "RealBoy6", "password": "Pinocchio"
-      }
+    it("should return a status 201 & the username on successful login", async () => {
       const response = await request(server).post("/api/auth/register").send(testee)
-      console.log(response.body)
+      // console.log(response.body)
       expect(response.status).toEqual(201)
       expect(response.body.username).toEqual(testee.username)
+    })
+    it("should return status 400 when given invalid registration", async () => {
+      const response = await request(server).post("/api/auth/register").send(failureTestee)
+      expect(response.status).toEqual(400)
+    })
+  })
+
+  describe("login endpoint", () => {
+    it("should return status 200 on a successful login", async () => {
+      const response = await request(server).post("/api/auth/login").send(testee)
+      expect(response.status).toEqual(200)
+    })
+    it("should return status 400 when given invalid login", async () => {
+      const response = await request(server).post("/api/auth/login").send(failureTestee)
+      expect(response.status).toEqual(400)
+    })
+  })
+
+  describe("jokes endpoint", () => {
+    it.skip("should return status 200 on a successful get", async () => {
+      const token = req.headers.authorization
+      const response = await request(server).post("/api/jokes").send(token)
+      console.log(response)
+      expect(response.status).toEqual(200)
+    })
+    it("should return status 400 when given invalid token", async () => {
+      const response = await request(server).post("/api/jokes").send(failureTestee)
+      expect(response.status).toEqual(401)
     })
   })
 })
