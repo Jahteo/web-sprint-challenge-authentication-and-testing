0fbd49a8fdf34b38a0e7870ca0cb1d9f3fb56c19
Josh Glantz-Hucks <jahteo@gmail.com>
2020-10-16 17:05:54 -0700

    improved .get(/status) to include db status, and set up for including status of other api's. fixing test next...

--- api/server.js
+++ api/server.js
@@ -15,8 +15,22 @@ server.use(express.json());
 server.use('/api/auth', authRouter);
 server.use('/api/jokes', authenticate, jokesRouter);
 
-server.get('/', (req, res) => {
-  res.status(200).json({ api: 'running' });
+server.get('/status', async (req, res) => {
+  //checking knex status too
+  res.status(200).json({
+    api: true,
+    db: await getDbStatus(),
+    //jokeApi: await getJokeStatus
+  });
 });
 
 module.exports = server;
+
+async function getDbStatus() {
+  try{
+    await knex.raw('select 1+1 as result');
+    return true;
+  } catch(err) {
+    return false;
+  }
+}
